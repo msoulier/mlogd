71d35cf132605b3973a612dd9a77f2ab5adfada2
Michael P. Soulier <msoulier@digitaltorque.ca>
2017-09-20 12:23:38 -0400

    Flushing test output

--- fake_input.py
+++ fake_input.py
@@ -5,7 +5,8 @@ import sys, datetime, time
 def main():
     while True:
         now = datetime.datetime.now()
-        print "The time is now %s" % now
+        sys.stdout.write("The time is now %s\n" % now)
+        sys.stdout.flush()
         time.sleep(1)
 
 if __name__ == '__main__':
